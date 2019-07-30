024fd107e7673fdfdbfa59362d63c4437f57d82e
myname21-meet <myname21@meet.mit.edu>
2019-07-30 10:26:04 -0400

    uploading

--- app.py
+++ app.py
@@ -3,7 +3,11 @@ app = Flask(__name__)
 
 @app.route('/')
 def home_page():
-    return "Hello World"
+	opposite_day = False
+	fav_foods = ['Hamburger', 'Steak', 'Panakook']
+	worse_foods = ['Tomatoes', 'Chocolate', 'Hummus']
+	return render_template("index.html", 
+		opposite_day = opposite_day, fav_foods = fav_foods, worse_foods = worse_foods)
 
 if __name__ == '__main__':
    app.run(debug = True)
